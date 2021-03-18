f561cd713766a2e0396b2345131d917e7a260f19
Mohamad Syauqi <b031910393@student.edu.my>
2021-03-18 19:12:43 +0800

    add more attribute

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,5 +3,24 @@ package samples.demo;
 public class Location {
 	
 
+	private String name;
+	private int locationId;
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name= name;
+	}
+	public int getLocationId()
+	{
+		return locationId;
+	}
+	public void setLocationId()
+	{
+		this.locationId = locationId;
+	}
 
 }
